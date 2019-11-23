d5b9232275aac8d2d2ed4f7e2c81881052687f41
Unix Sheikh <mail@unixsheikh.com>
2019-11-23 20:56:33 +0100

    Update whitelist

--- dnsmasq-blacklist.sh
+++ dnsmasq-blacklist.sh
@@ -259,10 +259,10 @@ sed -i -e 's/^0\.0.\0.\0 -/0.0.0.0 /g' *.txt
 #-------------------------------------#
 
 sed -i -e '/0.0.0.0 gleam.io/d' *.txt # Also remove the comment from notrack-blocklist.txt
-sed -i -e '/\.bit.ly$/d' *.txt
+sed -i -e '/0.0.0.0 bit.ly$/d' *.txt
 sed -i -e '/\.cloudflare.com$/d' *.txt
 sed -i -e '/\.streamelements.com$/d' *.txt
-sed -i -e '/\.googletagmanager.com$/d' *.txt
+sed -i -e '/0.0.0.0 googletagmanager.com$/d' *.txt
 sed -i -e '/\.googleapis.com$/d' *.txt
 sed -i -e '/\.one.com$/d' *.txt
 sed -i -e '/\.videolan.org/d' *.txt
